fa6b4e74103037d979922d3d3ec18e7a06410716
HassanAliMAli <hassanali205031@gmail.com>
2026-06-13 00:12:44 +0500

    chore: remove queues (paid), use free-tier cron worker

--- env.d.ts
+++ env.d.ts
@@ -4,10 +4,6 @@ interface Env {
   DB: D1Database;
   KV: KVNamespace;
   R2: R2Bucket;
-  MATCH_QUEUE: Queue;
-  NOTIFICATION_QUEUE: Queue;
-  CLEANUP_QUEUE: Queue;
-  DRAW_QUEUE: Queue;
   BETTER_AUTH_SECRET: string;
   RESEND_API_KEY?: string;
   ENVIRONMENT: string;
